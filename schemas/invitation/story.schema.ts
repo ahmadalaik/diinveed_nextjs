e25417e1d35454a3b6f987158deea0261e3740f6
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-04-02 13:25:43 +0700

    add: story schema

---
+++ schemas/invitation/story.schema.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const storySchema = z.object({
+  id: z.string(),
+  title: z.string().nonempty("Title is required").min(3, "Title is too short"),
+  date: z.iso.datetime({ error: "Date is required" }),
+  description: z
+    .string()
+    .nonempty("Description is required")
+    .min(3, "Description is too short"),
+  order: z.number(),
+});
+
+export type StorySchemaType = z.infer<typeof storySchema>;
