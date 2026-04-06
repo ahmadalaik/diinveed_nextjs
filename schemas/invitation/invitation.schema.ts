b4fddac4233af4a64595831ebf9fc332ad5cec02
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-04-06 22:30:47 +0700

    update: modify invitation schema

--- schemas/invitation/invitation.schema.ts
+++ schemas/invitation/invitation.schema.ts
@@ -1,39 +1,33 @@
 import z from "zod";
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+const imageValidation = z
+  .instanceof(File)
+  .refine((file) => {
+    if (!file) return true; // optional
+    return file.size <= MAX_FILE_SIZE;
+  }, "Maximum file size is 5MB.")
+  .refine((file) => {
+    if (!file) return true;
+    return ACCEPTED_IMAGE_TYPES.includes(file.type);
+  }, "Only .jpg, .jpeg, .png and .webp formats are supported.")
+  .optional();
+
 export const invitationSchema = z.object({
-  brideName: z
-    .string()
-    .nonempty("Bride name is required")
-    .min(3, "Bride name must be minimum 8 characters"),
-  brideNickname: z
-    .string()
-    .nonempty("Bride nickname is required")
-    .min(3, "Bride nickname must be minimum 8 characters"),
-  fatherBrideName: z
-    .string()
-    .nonempty("Father bride name is required")
-    .min(3, "Father bride name must be minimum 8 characters"),
-  motherBrideName: z
-    .string()
-    .nonempty("Mother bride name is required")
-    .min(3, "Mother bride name must be minimum 8 characters"),
+  bridePhoto: imageValidation,
+  brideName: z.string().min(1, "Bride name is required"),
+  brideNickname: z.string().min(1, "Bride nickname is required"),
+  fatherBrideName: z.string().min(1, "Father bride name is required"),
+  motherBrideName: z.string().min(1, "Mother bride name is required"),
   brideDescription: z.string().optional(),
-  groomName: z
-    .string()
-    .nonempty("Groom name is required")
-    .min(3, "Groom name must be minimum 8 characters"),
-  groomNickname: z
-    .string()
-    .nonempty("Groom nickname is required")
-    .min(3, "Groom nickname must be minimum 8 characters"),
-  fatherGroomName: z
-    .string()
-    .nonempty("Father groom name is required")
-    .min(3, "Father groom name must be minimum 8 characters"),
-  motherGroomName: z
-    .string()
-    .nonempty("Mother groom name is required")
-    .min(3, "Mother groom name must be minimum 8 characters"),
+
+  groomPhoto: imageValidation,
+  groomName: z.string().min(1, "Groom name is required"),
+  groomNickname: z.string().min(1, "Groom nickname is required"),
+  fatherGroomName: z.string().min(1, "Father groom name is required"),
+  motherGroomName: z.string().min(1, "Mother groom name is required"),
   groomDescription: z.string().optional(),
 });
 
