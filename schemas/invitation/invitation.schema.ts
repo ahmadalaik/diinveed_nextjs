c77f83e8c263113101ef853ccbb4adab0329c9f6
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-04-07 20:44:16 +0700

    update: modify photo in invitation schema

--- schemas/invitation/invitation.schema.ts
+++ schemas/invitation/invitation.schema.ts
@@ -1,29 +1,21 @@
 import z from "zod";
 
-const MAX_FILE_SIZE = 5 * 1024 * 1024;
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
-
-const imageValidation = z
-  .instanceof(File)
-  .refine((file) => {
-    if (!file) return true; // optional
-    return file.size <= MAX_FILE_SIZE;
-  }, "Maximum file size is 5MB.")
-  .refine((file) => {
-    if (!file) return true;
-    return ACCEPTED_IMAGE_TYPES.includes(file.type);
-  }, "Only .jpg, .jpeg, .png and .webp formats are supported.")
+const uploadedImageSchema = z
+  .object({
+    url: z.url(),
+    publicId: z.string(),
+  })
   .optional();
 
 export const invitationSchema = z.object({
-  bridePhoto: imageValidation,
+  bridePhoto: uploadedImageSchema,
   brideName: z.string().min(1, "Bride name is required"),
   brideNickname: z.string().min(1, "Bride nickname is required"),
   fatherBrideName: z.string().min(1, "Father bride name is required"),
   motherBrideName: z.string().min(1, "Mother bride name is required"),
   brideDescription: z.string().optional(),
 
-  groomPhoto: imageValidation,
+  groomPhoto: uploadedImageSchema,
   groomName: z.string().min(1, "Groom name is required"),
   groomNickname: z.string().min(1, "Groom nickname is required"),
   fatherGroomName: z.string().min(1, "Father groom name is required"),
