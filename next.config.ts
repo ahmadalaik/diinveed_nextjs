cc94f541ac5d47b59abca494d7060a7d8400c47f
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-04-07 20:40:42 +0700

    update: modify project config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://placehold.co/**"),
+      new URL(
+        "https://res.cloudinary.com/drcvunlct/image/upload/v1775494397/diinveed/users/**",
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
