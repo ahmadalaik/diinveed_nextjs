fb23528bd887b05d5ccf41c81a51cdff2aae5224
ahmadalaik <ahmadalaik.mu@gmail.com>
2026-04-08 23:02:34 +0700

    update: modify invitation schema

--- schemas/invitation/invitation.schema.ts
+++ schemas/invitation/invitation.schema.ts
@@ -1,26 +1,21 @@
 import z from "zod";
 
-const uploadedImageSchema = z
-  .object({
-    url: z.url(),
-    publicId: z.string(),
-  })
-  .optional();
-
-export const invitationSchema = z.object({
-  bridePhoto: uploadedImageSchema,
+export const invitationInfoSchema = z.object({
   brideName: z.string().min(1, "Bride name is required"),
   brideNickname: z.string().min(1, "Bride nickname is required"),
   fatherBrideName: z.string().min(1, "Father bride name is required"),
   motherBrideName: z.string().min(1, "Mother bride name is required"),
-  brideDescription: z.string().optional(),
+  brideDescription: z.string().nullable(),
+  bridePhotoUrl: z.string().nullable(),
+  bridePhotoPublicId: z.string().nullable(),
 
-  groomPhoto: uploadedImageSchema,
   groomName: z.string().min(1, "Groom name is required"),
   groomNickname: z.string().min(1, "Groom nickname is required"),
   fatherGroomName: z.string().min(1, "Father groom name is required"),
   motherGroomName: z.string().min(1, "Mother groom name is required"),
-  groomDescription: z.string().optional(),
+  groomDescription: z.string().nullable(),
+  groomPhotoUrl: z.string().nullable(),
+  groomPhotoPublicId: z.string().nullable(),
 });
 
-export type InvitationType = z.infer<typeof invitationSchema>;
+export type InvitationInfoType = z.infer<typeof invitationInfoSchema>;
